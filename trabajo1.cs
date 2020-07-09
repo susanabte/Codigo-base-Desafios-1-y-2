c8ef54b3c1dcd31819fde4b23e13212b16d7454c
susybte <susybusta2510@gmail.com>
2020-07-09 15:45:11 -0500

    Primeros pasos de git

---
+++ trabajo1.cs
@@ -0,0 +1,26 @@
+using System;using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		// Entrada del usuario de la variable X1
+		Console.Write("Ingrese X1: ");
+		double x1 = double.Parse(Console.ReadLine());
+		
+		// Entrada del usuario de la variable Y1
+		Console.Write("Ingrese Y1: ");
+		double y1 = double.Parse(Console.ReadLine());
+		
+		// Entrada del usuario de las variables X2
+		Console.Write("Ingrese X2: ");
+		double x2 = double.Parse(Console.ReadLine());
+
+        // Entrada del usuario de la variable Y1
+		Console.Write("Ingrese Y1: ");
+		double y1 = double.Parse(Console.ReadLine());
+	
+
+    }
+}
+					
